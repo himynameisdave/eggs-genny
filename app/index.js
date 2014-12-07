3f88aa27c313b8b78c41b486998dfd258f727c9d
Dave Lunny <dlunny@featurecreeptechnology.com>
2014-12-06 20:53:26 -0500

    some progress!
    Some progress! Actual scaffolding! Actual file creation with some fancy
    templating and stuff too!
    
    Next step is to bower up.

--- app/index.js
+++ app/index.js
@@ -3,10 +3,11 @@ var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
+var chalk = require( "chalk" );
 var banner = require('./banner.js');
 
 var EggsGennyGenerator = yeoman.generators.Base.extend({
-  promptUser: function() {
+    promptUser: function() {
         var done = this.async();
 
         // have our banner greet the user
@@ -18,6 +19,10 @@ var EggsGennyGenerator = yeoman.generators.Base.extend({
             name:    "name",
             message: "Now what did you say you were callin' this thing?",
             default: "egg"
+        },{
+            name:    "desc",
+            message: "Care to describe this application of yours?",
+            default: "Totally awesome rad app!"
         },{
             name:    "jquery",
             type:    "confirm",
@@ -43,17 +48,87 @@ var EggsGennyGenerator = yeoman.generators.Base.extend({
         this.prompt(prompts, function (props) {
 
             this.userInputs = props,
-            this.appName = props.appName;
+            this.appName = props.name;
+            this.desc = props.desc;
 
             done();
         }.bind(this));
     }, // end promptUser function
     scaffold: function(){
+        // for easier reffing
+        var UI = this.userInputs,
+            testString = 'Building you a sweet app with the following deps:\n';
+
+        //  check for jQuery:
+        if( UI.jquery ){
+            //do jquery stuff
+            testString += '\t- jQuery\n'
+        }
+
+        if( UI.angular ){
+            //do angular stuff
+            testString += '\t- Angular\n'
+        }
+
+        if( UI.gsap ){
+            //do gsap stuff
+            testString += '\t- GSAP\n'
+        }
+
+        if( UI.bootstrap ){
+            //do bootstrap stuff
+            testString += '\t- Bootstrap\n'
+        }
+
+        if( !UI.jquery && !UI.angular && !UI.gsap && !UI.bootstrap ){
+            testString += '\t<no dependencies>\n'
+            console.log(testString + chalk.red('Looks like someone\'s going bareback!\n') );
+        }
+
+    //  Make some directories
+        //  app/ directories
+        console.log(chalk.green("\n--  Creating app/ directories\n"));
+        this.mkdir("app");
+        this.mkdir("app/css");
+        this.mkdir("app/js");
+        //  temp/ directory, where files can be stored temporarily
+        this.mkdir("app/tmp");
+        this.mkdir("app/lib");
+
+        console.log(chalk.green("--  Creating build/ directories\n"));
+        //  build/ dir, for builds
+        this.mkdir("build");
+
+
+    },
+    copyFiles: function(){
+
+        var ctxt = {
+                appName: this.appName,
+                appDesc: this.desc,
+                deps: this.userInputs
+            };
+
+        //  package.json && gulpfile.js
+        if( this.userInputs.angular ){
+            this.template('_package.ang.json', "package.json", ctxt);
+            this.copy( '_gulpfile.ang.js', 'gulpfile.js' );
+        }else{
+            this.template('_package.json', "package.json", ctxt);
+            this.copy( '_gulpfile.js', 'gulpfile.js' );
+        }
+
+        //  main html file
+        this.template('app/_index.html', "app/index.html", ctxt);
 
 
 
 
+    },
+    end: function(){
+        console.log(chalk.blue("\n================================\n   Your eggs are ready, sir!   \n================================\n"));
     }
+
 });
 
 module.exports = EggsGennyGenerator;
