142cdb1bd7964271433b33ee6fc4fed8ce206c4e
Dave Lunny <dlunny@featurecreeptechnology.com>
2015-03-18 12:09:17 -0400

    adds crass to the package.json so it gets installed

--- app/index.js
+++ app/index.js
@@ -574,6 +574,7 @@ EggsGennyGenerator = yeoman.generators.Base.extend({
                 "gulp-jshint": "~1.9.0",
                 "gulp-livereload": "~3.0.2",
                 "gulp-load-plugins": "~0.8.0",
+                "gulp-crass": "~0.1.2",
                 "gulp-minify-css": "~0.3.11",
                 "gulp-uglify": "~1.0.2",
                 "gulp-uncss": "~0.5.2"
