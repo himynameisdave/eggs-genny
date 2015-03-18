f5d2b19cc538c527c481096821221b85356d6a0a
Dave Lunny <dlunny@featurecreeptechnology.com>
2015-03-18 12:45:38 -0400

    adds crass to gulpfile
    tested and works better than minifycss.
    
    Removes minify-css from the generated package.json

--- app/index.js
+++ app/index.js
@@ -575,7 +575,6 @@ EggsGennyGenerator = yeoman.generators.Base.extend({
                 "gulp-livereload": "~3.0.2",
                 "gulp-load-plugins": "~0.8.0",
                 "gulp-crass": "~0.1.2",
-                "gulp-minify-css": "~0.3.11",
                 "gulp-uglify": "~1.0.2",
                 "gulp-uncss": "~0.5.2"
               }
